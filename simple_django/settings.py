0126f2c2900fba0e263851b14742e2c63afa652e
Sureshgh197 <srshbtt@gmail.com>
2022-03-05 18:20:25 +0530

    profile image

--- simple_django/settings.py
+++ simple_django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bx!@+7rh^a2wx&z@b6!d4zm1*7ztj1=-bnff6xo9z_2)bmtdy8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
